194bfa7d8c2d787327486ce21ba56b4e7c18400a
Duarte <duarteamelo@gmail.com>
2018-04-11 20:34:29 +0100

    Classe soma de potencias

--- src/pa/calc/SomaDePotencias.java
+++ src/pa/calc/SomaDePotencias.java
@@ -5,6 +5,6 @@ public class SomaDePotencias implements ItfOper {
     @Override
     public double oper(double op1, double op2) {
         return op1*op1 + op2*op2 ;
-    } 
+    }  
                     
 }
