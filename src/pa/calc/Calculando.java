8bb5e6e7cb7979252c54aa3c906a41b4dcf73f0a
SusanaJesus <susana.carrilho.ramos@gmail.com>
2018-04-11 22:07:15 +0100

    class Calculando

---
+++ src/pa/calc/Calculando.java
@@ -0,0 +1,9 @@
+package pa.calc;
+
+public class Calculando implements ItfOper {
+	@Override
+		public double oper(double firstno, double secondno) {
+	return (firstno*53+(secondno+35))/5335;
+	
+}
+}
