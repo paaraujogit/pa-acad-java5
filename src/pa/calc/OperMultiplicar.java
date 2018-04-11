e3a8089ecbabdb5d5ca1e366ee4e631cdb8dc74e
André Francisco <>
2018-04-11 20:21:59 +0100

    Actualização da Calculadora (Operação Multiplicar)

---
+++ src/pa/calc/OperMultiplicar.java
@@ -0,0 +1,10 @@
+package pa.calc;
+
+public class OperMultiplicar implements ItfOper {
+    
+    @Override
+    public double oper(double arg1, double arg2) {
+        return arg1*arg2;
+    }
+                    
+}
